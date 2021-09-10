81543881efb6a0ebe1215b22a11392b05005a467
Marc Hoersken <marc.hoersken@thyssenkrupp.com>
2021-09-10 11:48:25 +0200

    Fix bbcode output as detected by new CI checks

--- pf_focus/bbcode.py
+++ pf_focus/bbcode.py
@@ -109,7 +109,7 @@ def output_bbcode(doc, stream):
 
     stream.write(h2("System\n"))
     info = obj_to_dict(doc.pfsense.system, ('hostname', 'domain', 'timeservers', 'timezone', 'language', 'dnsserver'))
-    info['dnsserver'] = ', '.join(info['dnsserver'])
+    info['dnsserver'] = ', '.join(map(format_bbcode_cell, info['dnsserver']))
     output_bbcode_table(stream, ('Option', 'Value'), info.items())
     stream.write("\n")
 
